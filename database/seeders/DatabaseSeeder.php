1714c4173e1e6ac56383bd653140b1cf2264008f
arjun-ux <arux899@gmail.com>
2025-07-30 20:36:49 +0700

    fix: sample seeder commenr

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            SampleDataSeeder::class,
+            // SampleDataSeeder::class,
         ]);
     }
 }
