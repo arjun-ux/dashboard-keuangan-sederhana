d7d4d57979168157f6f30eeadbe4f6d9c532140d
arjun-ux <arux899@gmail.com>
2025-07-30 21:10:16 +0700

    tanpa node js

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,6 +24,7 @@
     <title>@yield('title', 'Financial Management')</title>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <style>
